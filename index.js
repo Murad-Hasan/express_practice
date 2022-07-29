f77f7e3c4e289f53b578f673f0b0c0755944650e
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-07-30 01:42:41 +0600

    controller

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const express = require("express");
 
-
 const cors = require("cors");
 const morgan = require("morgan");
 
-const app = express();//router mainly a middleware
+const app = express(); //router mainly a middleware
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
 app.use(morgan("dev"));
-app.use(globalMiddleware);
+// app.use(globalMiddleware);
 app.use(require("./routes"));
 
+app.use((req, res, next) => {
+  const error = new Error("404 not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status).send(error.message);
+  }
+  res.status(500).send("something went wrong");
+});
+
 // Global middleware create
-function globalMiddleware(req, res, next) {
-  console.log(`${req.method} - ${req.url}`);
-  console.log("Global Middleware");
-  if (req.query.bad) res.status(400).send("Something went wrong");
-  next();
-}
+// function globalMiddleware(req, res, next) {
+//   console.log(`${req.method} - ${req.url}`);
+//   console.log("Global Middleware");
+//   if (req.query.bad) res.status(400).send("Something went wrong");
+//   next();
+// }
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
--- routes.js
+++ routes.js
@@ -6,27 +6,21 @@ const {
 } = require("./controller");
 
 const router = require("express").Router();
-
 router.get("/", homeController);
-
 /*
   Route Specific middleware
   app.get("/about", cors(), (req, res) => {} // single middleware
     or,
   app.get("/about", [cors(), cors()], (req, res)) => {} // array of middleware
 */
-
 router.get("/about", localMiddleware, aboutController);
-
 //Local middleware create for about page
-
 function localMiddleware(req, res, next) {
   console.log("Local Middleware for about page");
   next();
 }
-
 router.get("/contact", contactController);
-
 router.get("/help", helpController);
+
 
 module.exports = router;
